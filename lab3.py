b03b773bd05710a8b861bd680e6e1b5671154e90
Chris <osh1996@gmail.com>
2017-04-05 17:36:01 -0400

    just need to finish astar

--- lab3.py
+++ lab3.py
@@ -1,16 +1,24 @@
 import rospy, math, tf
 from geometry_msgs.msg import PoseStamped
 from tf.transformations import euler_from_quaternion
-from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import OccupancyGrid, GridCells
 
 class Node:
-
 	def __init__(self, x, y, gCost, fCost, cameFrom):
 		self.x = x
 		self.y = y
 		self.gCost = gCost
 		self.fCost = fCost
 		self.cameFrom = cameFrom
+
+	def calcgCost(self):
+		return manhattan(self)
+
+	def calchCost(self):
+		return eucl(self)
+	
+	def calcfCost(self):
+		return manhattan(self) + eucl(self)
 
 #returns data (probability) of map cell given (x,y) coord
 def getCellValue(x,y):
@@ -19,22 +27,38 @@ def getCellValue(x,y):
     #get map info
     cols = mapdata.info.width
     rows = mapdata.info.height
+   
+    index = (y * cols) + x #zero indexed, (y * cols) represents first number of each row, then add x (the column)
+    return mapdata[index] 
+
+def pointToIndex(x,y):
+    global mapdata
+    
+    #get map info
+    cols = mapdata.info.width
+    rows = mapdata.info.height
     #res = grid.info.resolution #m/cell, might come in handy later
    
     index = (y * cols) + x #zero indexed, (y * cols) represents first number of each row, then add x (the column)
-    return mapdata(index) 
-        
-    
-    index = 
-    
-    #get grid info
-    rows = grid.info.height
-    cols = grid.info.width
-    res = grid.info.resolution
+    return index 
+
+
+def indexToPoint(index):
+	global mapdata
+	#gets map info
+	cols = mapdata.info.width
+	rows = mapdata.info.height
+
+	y = index % cols
+	x = index // cols
+	node = node()
+	node.x = x
+	node.y = y
+	return node
 
 
 def astar(start, goal):													# returns zero if no path from start to goal otherwise returns path from start to goal
-	
+	pass
 	global current
 
 	current = start														
@@ -48,9 +72,9 @@ def astar(start, goal):													# returns zero if no path from start to goal
 	gCost[start] = 0       												# Have not travelled when at start, thererfore inits as zero
 	fCost[start] = eucl(start, goal)									# The cost of going start to goal by passing that node
 
-	while(openSet is not None):
+	while openSet:
 
-		current = ###########3											# goes to the node in openset having the lowest fCost
+		current = start														# goes to the node in openset having the lowest fCost
 
 		if(current is goal):											# if current is goal ->
 			return repath(current) 										# then return the path back to the start
@@ -68,7 +92,7 @@ def astar(start, goal):													# returns zero if no path from start to goal
 			if neighbor not in openSet: 								# discovered a new node
 				openSet.add(neighbor) 	
 
-			else if tentativegCost >= gCost[neighbor]: 					# this is not a better path
+			elif (tentativegCost >= gCost[neighbor]): 					# this is not a better path
 				continue
 
 			cameFrom[neighbor] = current
@@ -76,27 +100,30 @@ def astar(start, goal):													# returns zero if no path from start to goal
 			fcost[neighbor] = gCost[neighbor] + eucl(neighbor, goal)
 
 		return "Error in ASTAR"
-
-def manhattan(node, goal):												# returns the manhattan distance
-	x = abs(goal.x - node.x)
-	y = abs(goal.y - node.y)
+	##pass
+def manhattan(node):												# returns the manhattan distance
+	global start
+	x = abs(start.x - node.x)
+	y = abs(start.y - node.y)
 	return x + y
 
 def repath(node):
 	global path
-	path = p[]
+	path = []
 	last = node.CameFrom
 	while(last is not 0):
-		p.add(last)
+		path.add(last)
 	return path
 
-def eucl(node, neighbor):												# distance between 2 nodes
+def eucl(node):												# distance between node and goal
+	global goal
 	ix = node.x
 	iy = node.y
-	nx = neighbor.x
-	ny = neighbor.y
+	nx = goal.x
+	ny = goal.y
 	dist = math.sqrt((nx-ix)**2 + (ny - iy)**2)
 	return dist
+
 
 
 
@@ -115,10 +142,10 @@ def eucl(node, neighbor):												# distance between 2 nodes
 # 	return graph
 
 
-def goal(msg):															# stores goal pose as global			
-	global goal
+def gl(msg):															# stores goal pose as global			
+	global gl
 	if msg:
-		goal = msg.pose
+		gl = msg.pose
 
 def ipose(msg):															# stores start pose as global
 	global ipose
@@ -140,13 +167,28 @@ def ogrid(msg):															# stores the elements of GridCells as a global
 	if msg:
 		mapdata = msg.data
 
+
+
+
+
+
 def main():																# main function
-	while not rospy.isShutdown():
-		rospy.sleep()
+	start = Node(0, 0, 0, 0, 0)
+	start.calcfCost
+	var = Node(1, 1, 2, 0, start)
+	var.calcfCost
+	goal = Node(2, 2, 4, 4, var)
+	print "start fcost", start.fCost
+	print "gCost", var.gCost
+	print "fCost", var.fCost
+
+
+
 
 
 if __name__ == '__main__':
 	global goal
+	global start
 	global spose
 	global GridCells
 	global mapdata
@@ -159,9 +201,9 @@ if __name__ == '__main__':
 	
 
 ########SUBSCRIBERS
-	goal_sub = rospy.Subscribe('/navGoal', PoseStamped, goal, queue_size=1)
-	init_sub = rospy.Subscribe('/initialPose', PoseStamped, ipose, queue_size=1)
-	map_sub = rospy.Subscribe('/map', OccupancyGrid, ogrid, queue_size=1)
+	goal_sub = rospy.Subscriber('/navGoal', PoseStamped, gl, queue_size=1)
+	init_sub = rospy.Subscriber('/initialPose', PoseStamped, ipose, queue_size=1)
+	map_sub = rospy.Subscriber('/map', OccupancyGrid, ogrid, queue_size=1)
 
 	rospy.sleep(rospy.Duration(1, 0))
 
