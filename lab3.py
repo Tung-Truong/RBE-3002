4d7d500d7c0e983b74a4ab2d1201ada25640dbf6
Chris <osh1996@gmail.com>
2017-04-04 23:31:35 -0400

    more changes to astar

--- lab3.py
+++ lab3.py
@@ -1,20 +1,14 @@
 import rospy, math
+from geometry_msgs.msg import PoseStamped
 
 class Node:
 
-	def __init__(self, x, y, gCost, cameFrom):
+	def __init__(self, x, y, gCost, fCost, cameFrom):
 		self.x = x
 		self.y = y
 		self.gCost = gCost
+		self.fCost = fCost
 		self.cameFrom = cameFrom
-
-	def calcgCost(self):												# if at start return zero otherwise return 1
-		if(self.gCost == 0):
-			return 0
-		return 1
-	
-	def calcgCost(self):
-		self.gCost = ######
 
 
 def astar(start, goal):													# returns zero if no path from start to goal otherwise returns path from start to goal
@@ -35,12 +29,12 @@ def astar(start, goal):													# returns zero if no path from start to goal
 		current = min()													# goes to the node in openset having the lowest fCost
 
 		if(current is goal):											# if current is goal ->
-			return repath(cameFrom, current) 							# then return the path back to the start
+			return repath(current) 										# then return the path back to the start
 
 		openSet.remove(current) 										# update each of the sets
 		closedSet.add(current)
 
-		for neighbor in range(0, current.neighbors()):
+		for neighbor in neighbors:
 			
 			if neighbor in closedSet: 									# ignores already evaluated nodes
 				continue
@@ -57,15 +51,20 @@ def astar(start, goal):													# returns zero if no path from start to goal
 			gCost[neighbor] = tentativegCost
 			fcost[neighbor] = gCost[neighbor] + eucl(neighbor, goal)
 
-		return 0
+		return "Error in ASTAR"
 
 def manhattan(node, goal):												# returns the manhattan distance
 	x = abs(goal.x - node.x)
 	y = abs(goal.y - node.y)
 	return x + y
 
-def repath(came, node):
-	pass
+def repath(node):
+	global path
+	path = p[]
+	last = node.CameFrom
+	while(last is not 0):
+		p.add(last)
+	return path
 
 def eucl(node, neighbor):												#distance between 2 nodes
 	ix = node.x
@@ -74,8 +73,6 @@ def eucl(node, neighbor):												#distance between 2 nodes
 	ny = neighbor.y
 	dist = math.sqrt((nx-ix)**2 + (ny - iy)**2)
 	return dist
-
-
 
 
 def mkGraph(rows, columns, inits): 		# makes a graph with specified number of rows and columns and initializes it to (inits)
@@ -93,34 +90,33 @@ def mkGraph(rows, columns, inits): 		# makes a graph with specified number of ro
 
 
 
-## three publishers 
-	#path
-
-## three subscribers
-	#initalpose
-	#2dnavgoal
-	#map
-
-
+def main():
+	while not rospy.isShutdown():
+		rospy.sleep()
 
 
 if __name__ == '__main__':
+	global spose
+	global GridCells
+	spose = PoseStamped()
+	GridCells = GridCells()
+
+
 	rospy.init_node("lab3")
 
 ########PUBLISHERS
-
+	
 
 ########SUBSCRIBERS
-	goal_sub = rospy.Subscribe('/navGoal', queue_size=1)
-	init_sub = rospy.Subscribe('/initialPose', queue_size=1)
-	map_sub = rospy.Subscribe('/map')
+	goal_sub = rospy.Subscribe('/navGoal', PoseStamped, queue_size=1)
+	init_sub = rospy.Subscribe('/initialPose', PoseStamped, queue_size=1)
+	map_sub = rospy.Subscribe('/map', GridCells, queue_size=1)
 
 	rospy.sleep(rospy.Duration(1, 0))
 
 
 	print "begin main"
 	
-	while not rospy.isShutdown():
-		rospy.sleep()
+	main()
 
 	print "end main"
